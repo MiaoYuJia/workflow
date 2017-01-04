80edb1c7204a625490eb39178bece2762b13a8c1
Yujia.Miao <yujia.miao@ipinyou.com>
2017-01-04 16:53:03 +0800

    miao

--- hive_script/hcreate.sh
+++ hive_script/hcreate.sh
@@ -3,12 +3,18 @@ tname=$1
 tcol=$2
 tlocation=$3
 
+if [ "${#@}" != 3 ];then
+        echo "Error : $(basename $0) 请输入4个参数：tname, tcol, tlocaltion"
+        exit -1
+fi 
+
+
 tcols=`echo $2 | awk 'BEGIN {FS="#"; OFS="\n"}  END{ for(i=1;i<=NF;i++)  if(i!=NF)  {print $i " string," } else {print $i " string"}  }'`
 
 sql="use yujia;CREATE EXTERNAL TABLE $tname(
  $tcols )
 ROW FORMAT DELIMITED
-  FIELDS TERMINATED BY ','
+  FIELDS TERMINATED BY '\t'
 STORED AS INPUTFORMAT
   'org.apache.hadoop.mapred.TextInputFormat'
 OUTPUTFORMAT
